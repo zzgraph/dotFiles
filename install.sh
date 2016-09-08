4954f41c0eb09aecaaa492dbc704a31b373a568a
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2016-09-08 17:38:06 +0430

    Creating install file to move files
    .vim folder and .vimrc files has been added to install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+if [ -e $HOME/.vimrc ]; then
+    echo "Your previous vim configuration file "$HOME"/.vimrc \
+    	 has been renamed to "\
+	 $HOME"/.vimrc.back".$(date +%Y-%m-%d_%H:%M:%S);
+    
+    mv $HOME/.vimrc $HOME/.vimrc.back.$(date +%Y-%m-%d_%H:%M:%S)
+fi
+
+if [ -d $HOME/.vim ] && [ -d $HOME/.vim ]; then
+    echo "Your previous vim configuration file "$HOME"/.vim \
+    	 has bee renamed to "\
+	 $HOME"/.vim.back."$(date +%Y-%m-%d_%H:%M:%S);
+    
+    mv $HOME/.vimrc $HOME/.vim.back.$(date +%Y-%m-%d_%H:%M:%S)
+fi
+
+mv ./vimrc ~/.vimrc
+mv ./vim/ ~/.vim
