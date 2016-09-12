dc0488d967dde8699649bc3d5c2fcdf3df8c9014
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2016-09-12 19:45:14 +0430

    Properly install bin folder

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 declare -a configFiles=(
-"bashrc"
-"vimrc"
-"dir_colors"
-"vim"
-"bash_profile"
-"bashrc.d"
-"mlterm"
-"bin"
+    "bashrc"
+    "vimrc"
+    "dir_colors"
+    "vim"
+    "bash_profile"
+    "bashrc.d"
+    "mlterm"
+)
+declare -a noDotConfig=(
+    "bin"
 )
 
 
@@ -32,7 +34,33 @@ for j in ${!configFiles[*]}; do
     fi;
 done
 
+for j in ${!noDotConfig[*]}; do
+    if [ -h $HOME/${noDotConfig[$j]} ]; then
+        echo "Moving previously linked configuration files to $HOME/rcfiles.backup and create a new link";
+        if [ ! -d $HOME/.rcfiles.backup ]; then
+            mkdir $HOME/.rcfiles.backup;
+        fi
+        mv $HOME/${noDotConfig[$j]} $HOME/.rcfiles.backup/${noDotConfig[$j]}.back.$(date +%Y-%m-%d_%H:%M:%S);
+
+    elif [ -e $HOME/${noDotConfig[$j]} ] || [ -d $HOME/${noDotConfig[$j]} ]; then
+
+        echo "Your previous configuration file $HOME/.${noDotConfig[$j]}";
+        echo "has been renamed to $HOME/${noDotConfig[$j]}.back$(date +%Y-%m-%d_%H:%M:%S)";
+
+        if [ ! -d $HOME/.rcfiles.backup ]; then
+            mkdir $HOME/.rcfiles.backup;
+        fi
+        mv $HOME/${noDotConfig[$j]} $HOME/.rcfiles.backup/${noDotConfig[$j]}.back.$(date +%Y-%m-%d_%H:%M:%S);
+
+    fi;
+done
+
 for i in ${!configFiles[*]}; do
     echo "Linking new configuration file ${configFiles[$i]} to your home directory";
     ln -s $PWD/${configFiles[$i]} $HOME/.${configFiles[$i]};
+done
+
+for i in ${!noDotConfig[*]}; do
+    echo "Linking new configuration file ${noDotConfig[$i]} to your home directory";
+    ln -s $PWD/${noDotConfig[$i]} $HOME/${noDotConfig[$i]};
 done
